ffaebd1996717cc477b7a8aff3feb4f69104ee98
Davin <davin.hayden@outlook.com>
2020-09-28 12:27:59 +0100

    added basic structure to index file from  wireframe, plus basic css style

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Collector's App</title>
+    <link href="normalize.css" type="text/css" rel="stylesheet">
+    <link href="collection_style.css" type="text/css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+<header>
+    <h1>Collectors App</h1>
+</header>
+
+<main>
+    <div></div>
+    <section class="layout__collections">
+        <article class="container__film">
+            <!--            <article class=" section__box-emphasis">-->
+            <h2>Film</h2>
+            <p>Name</p>
+            <p>Year Produced fgdfdgdgdsfgdfgdfgdfg d dfgdsfgsdfsgdfgdffg</p>
+            <p>Genre</p>
+            <p>Roles</p>
+            <!--                <div>-->
+            <!--                    <p class="title-subheading">Web Developer in UK & NZ</p>-->
+            <!--                </div>-->
+            <!--            </article>-->
+        </article>
+        <article class="container__film">
+            <!--            <article class=" section__box-emphasis">-->
+            <h2>Film</h2>
+            <p>Name</p>
+            <p>Year Produced fgdfdgdgdsfgdfgdfgdfg d dfgdsfgsdfsgdfgdffg</p>
+            <p>Genre</p>
+            <p>Roles</p>
+            <!--                <div>-->
+            <!--                    <p class="title-subheading">Web Developer in UK & NZ</p>-->
+            <!--                </div>-->
+            <!--            </article>-->
+        </article>
+    </section>
+
+    <section class="layout__add-items">
+        <a href="change.php">Add Film</a>
+    </section>
+</main>
+
+
+<footer>
+    <p>&copy Davin Hayden 2020</p>
+</footer>
+
+
+</body>
+</html>
