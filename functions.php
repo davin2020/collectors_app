2688b5c490244c39d28ed639271c841f64900712
Davin <davin.hayden@outlook.com>
2020-09-28 15:51:29 +0100

    added functions getAllRolesForFilms and displayFilmsAndRoles and updated index to include them, also changed colors in css

--- functions.php
+++ functions.php
@@ -36,9 +36,11 @@ function getAllRolesForFilms(string $db_name):  array {
     $db = new PDO($dsn,'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-    $query = $db->prepare('select `films`.`id`, `title`, `year_produced`, `type` from `films` 
-join `genre`
-on `films`.`genre` = `genre`.`id`' );
+    $query = $db->prepare('select `films`.`id` as `film-id`, `name`, `roles`.`id` from `films` 
+join `film_roles`
+on `films`.`id` = `film_roles`.`film_id` 
+join `roles`
+on `roles`.`id` = `film_roles`.`role_id`' );
     $query->execute();
 
     $result = $query->fetchAll();
@@ -46,7 +48,7 @@ on `films`.`genre` = `genre`.`id`' );
 
 }
 
-function displayFilmsWithoutRoles($result){
+function displayFilmsWithoutRoles($result): string{
     $film_results = "";
     foreach($result as $key => $value) {
         $film_results .= '<article class="container__film">';
@@ -61,5 +63,28 @@ function displayFilmsWithoutRoles($result){
     return $film_results;
 }
 
+function displayFilmsAndRoles($result_films, $result_roles): string{
+    $film_results = "";
+    foreach($result_films as $key => $value) {
+        $film_results .= '<article class="container__film">';
+        $film_results .= '<br><br>';
+        $film_results .= '<h2>Film: ' . $result_films[$key]['title'] . '</h2>' ;
+        $film_results .= '<p>ID: ' . $result_films[$key]['id'] . '</p>';
+        $film_results .= '<p>Year Produced: ' .  $result_films[$key]['year_produced'] . '</p>';
+        $film_results .= '<p>Genre: ' . $result_films[$key]['type'] . '</p>';
+        $film_results .= '<p>My Roles: </p>';
+        $film_results .= '<ul>';
+        foreach($result_roles as $key_role => $value_role) {
+//            $film_results .= $result_roles[$key_role]['film-id'];
+            if ($result_films[$key]['id'] == $result_roles[$key_role]['film-id']) {
+                $film_results .= '<li>' . $result_roles[$key_role]['name']   . '</li>' ;
+            }
+        }
+        $film_results .= '</ul>';
+//        $film_results .= '<br><br>';
+        $film_results .= '</article>';
+    }
+    return $film_results;
+}
 
 ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require ('functions.php');
 
 <head>
     <meta charset="utf-8">
-    <title>Collector's App</title>
+    <title>Collection of  Short Films I've Worked On</title>
     <link href="normalize.css" type="text/css" rel="stylesheet">
     <link href="collection_style.css" type="text/css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,52 +16,38 @@ require ('functions.php');
 <body>
 
 <header>
-    <h1>Collectors App</h1>
+    <h1>Collection of  Short Films I've Worked On</h1>
 </header>
 
 <main>
     <div></div>
     <section class="layout__collections">
+
+        <!--
         <article class="container__film">
-            <!--            <article class=" section__box-emphasis">-->
             <h2>Film</h2>
             <p>Name</p>
             <p>Year Produced:</p>
             <p>Genre</p>
             <p>Roles</p>
-            <!--                <div>-->
-            <!--                    <p class="title-subheading">Web Developer in UK & NZ</p>-->
-            <!--                </div>-->
-            <!--            </article>-->
         </article>
+        -->
 
             <?php
-            //            $result =  getAllFilms();
-            $result =  getAllFilmsWithoutRoles('collectors_db');
-            var_dump($result);
-            echo '<br><br>';
+            $result_films =  getAllFilmsWithoutRoles('collectors_db');
+//            var_dump($result_films);
+//            echo displayFilmsWithoutRoles($result_films);
 
-            $temp_string = displayFilmsWithoutRoles($result);
-            var_dump($temp_string);
+            $result_roles =  getAllRolesForFilms('collectors_db');
+            echo displayFilmsAndRoles($result_films, $result_roles);
 
-//            foreach($result as $key => $value) {
-//                echo '<article class="container__film">';
-//                echo '<br><br>';
-//                echo '<h2>Film: ' . $result[$key]['title'] . '</h2>' ;
-//                echo '<p>ID: ' . $result[$key]['id'] . '</p>';
-//                echo '<p>Year Produced: ' .  $result[$key]['year_produced'] . '</p>';
-//                echo '<p>Genre: ' . $result[$key]['type'] . '</p>';
-//                echo '<br><br>';
-//                echo '</article>';
-//            }
-//            var_dump($result);
             ?>
 
-
     </section>
-
+    <section class="layout__collections">
     <section class="layout__add-items">
-        <a href="change.php">Add Film</a>
+        <div><a href="change.php">Add Film</a></div>
+    </section>
     </section>
 </main>
 
