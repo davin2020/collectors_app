80348f288b568672c5d83e2bca8a0e7526a3b5ca
Davin <davin.hayden@outlook.com>
2020-09-30 14:16:07 +0100

    tidied up comments & code formatting in index & functions files

--- functions.php
+++ functions.php
@@ -13,44 +13,40 @@ function getDBConnection(string $db_name): object {
 }
 
 /**
- * Function to get all the films in the collection, but none of the associated roles
+ * Function to get all the films in the collection, but none of the associated roles, where the film is not flagged as being deleted
  * The DB contains a table of films, plus a linked table of multiple roles/jobs that I have undertaken on each file
  */
 function getAllFilmsWithoutRoles(object $db):  array
 {
-    $query = $db->prepare('select `films`.`id`, `title`, `year_produced`, `type` from `films` 
-        join `genre`
-        on `films`.`genre` = `genre`.`id` 
-        where `is_deleted` = false' );
+    $query = $db->prepare('SELECT `films`.`id`, `title`, `year_produced`, `type` FROM `films` 
+        JOIN `genre`
+        ON `films`.`genre` = `genre`.`id` 
+        WHERE `is_deleted` = false' );
     $query->execute();
     $result = $query->fetchAll();
     return $result;
 }
 
 /**
- * Function to get all the roles I have done, for each of the films in the collection
+ * Function to get all the roles I have done, for each of the films in the collection, where the film is not flagged as being deleted.
  * The DB contains a table of films, plus a linked table of multiple roles/jobs that I have undertaken on each file
  */
 function getAllRolesForFilms(object $db):  array
 {
-    $query = $db->prepare('select `films`.`id` as `film-id`, `name`, `roles`.`id` from `films` 
-        join `film_roles`
-        on `films`.`id` = `film_roles`.`film_id` 
-        join `roles`
-        on `roles`.`id` = `film_roles`.`role_id` 
-        where `is_deleted` = false' );
+    $query = $db->prepare('SELECT `films`.`id` AS `film-id`, `name`, `roles`.`id` FROM `films` 
+        JOIN `film_roles`
+        ON `films`.`id` = `film_roles`.`film_id` 
+        JOIN `roles`
+        ON `roles`.`id` = `film_roles`.`role_id` 
+        WHERE `is_deleted` = false' );
     $query->execute();
     $result = $query->fetchAll();
     return $result;
 }
 
-/**
+/** REMOVE THIS
  * Function to display all the films in the collection, and for each film, display all the roles that I did for that flm
  */
-//foreach($array as $values) {
-//if(isset($values['year']))
-
-
 function displayFilmsAndRolesOLD(array $result_films, array $result_roles): string
 {
     $film_results = "";
@@ -77,41 +73,35 @@ function displayFilmsAndRolesOLD(array $result_films, array $result_roles): stri
 
 }
 
+/**
+ * Function to display all the films in the collection, and for each film, display all the roles that I did for that film. If film id or title is missing, dont show any details of the film and move onto showing the next film
+ */
 function displayFilmsAndRoles(array $result_films, array $result_roles): string
 {
     $film_results = "";
-//    var_dump($result_films);
-
-    //result film is outside array, key value is for elements inside
-//    foreach($result_films as $key => $value) {
-    //CHECK if keys exist first, then make empty str if dont exist - doesnt work for 2D arrays array_key_exists
+    //iterate over outer array of films, and show each film
     foreach($result_films as $film) {
-//        if (array_key_exists('id', $film) && array_key_exists('title', $film) && ($film['title'] != "" ) ) {
-//            echo '<br><br>FOUND KEY ID + TITLE using KEY EXISTS ';
-//        }
-
-        //does key ie  var exist, rather than is it null?
-        //but if its null it returns false & thus doesnt get shown if i break out of loop?
-//        if(isset($film['id']) && isset($film['title']) && ($film['title'] != "" )) {
-
-        if (array_key_exists('id', $film) && array_key_exists('title', $film) && ($film['title'] != "" ) ) {
-            echo '<br><br>FOUND ID & TITLE';
-            $film_results .= '<article class="container__film">';
-            // if titles empty it still shows a empty blue box
-                    $film_results .= '<h2>Film: ' . $film['title'] . '</h2>' //doesnt show title if null but shows it if its blank
-                    . '<p>ID: ' . $film['id'] . '</p>'
-                    . '<p>Year Produced: ' . $film['year_produced'] . '</p>'
-                    . '<p>Genre: ' . $film['type'] . '</p>'
-                    . '<p>My Roles: </p>'
-                    . '<ul>';
-                    foreach ($result_roles as $film_role) {
-                        if ($film['id'] == $film_role['film-id']) {
-                            $film_results .= '<li>' . $film_role['name'] . '</li>';
-                        }
-                    }
-                    $film_results .= '</ul>'
-                        . '</article>';
-        } else { //if a film id or title is missing,skip that film and move onto the next one
+        //check that mandatory fields id (auto-incremented primary key) and title are present & not null...
+        if ( array_key_exists('id', $film)
+            && array_key_exists('title', $film)
+            && ($film['title'] != "") )
+        {
+            $film_results .= '<article class="container__film">'
+                . '<h2>Film: ' . $film['title'] . '</h2>'
+//                . '<p>ID: ' . $film['id'] . '</p>'
+                . '<p>Year Produced: ' . $film['year_produced'] . '</p>'
+                . '<p>Genre: ' . $film['type'] . '</p>'
+                . '<p>My Roles: </p>'
+                . '<ul>';
+            // iterate over inner array of film roles, and show each role where the film id matches
+            foreach ($result_roles as $film_role) {
+                if ($film['id'] == $film_role['film-id']) {
+                    $film_results .= '<li>' . $film_role['name'] . '</li>';
+                }
+            }
+            $film_results .= '</ul>'
+                . '</article>';
+        } else { // ...else if a film id or title is missing, skip that film and move onto the next one
             $film_results .= "";
         }
     }
--- index.php
+++ index.php
@@ -20,30 +20,11 @@ require ('functions.php');
     <section class="layout__collections">
             <?php
             $db_name = 'collectors_db';
-//            echo '<br><br>NEW<br>';
             $db = getDBConnection($db_name);
-//            echo '<br><br>';
-//            var_dump($db);
-
-            echo '<br><br>';
             $result_films =  getAllFilmsWithoutRoles($db);
-//            echo '<br><br>result_films<br>';
-//            var_dump($result_films);
-
-            echo '<br><br>';
             $result_film_roles =  getAllRolesForFilms($db);
-//            echo '<br><br>result_film_roles<br>';
-//            var_dump($result_film_roles);
-
-//            echo '<br><br>';
-//            echo displayFilmsAndRolesOLD($result_films, $result_film_roles);
-
-            echo '<h3>NEW displayFilmsAndRolesNEW</h3>';
+//            echo '<h3>NEW displayFilmsAndRolesNEW</h3>';
             echo displayFilmsAndRoles($result_films, $result_film_roles);
-
-//            echo 'vardump';
-//            $tmp = displayFilmsAndRolesNEW($result_films, $result_film_roles);
-//            var_dump($tmp);
             ?>
     </section>
     <section class="layout__button-area">
